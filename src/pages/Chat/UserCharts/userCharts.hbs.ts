3fa3532e927725f858c0da2dbcaebedae99b6bbf
sergey.antonov <sergey.antonov@nedra.digital>
2022-11-19 09:09:16 +0300

    Sprint 3-1

--- src/pages/Chat/UserCharts/userCharts.hbs.ts
+++ src/pages/Chat/UserCharts/userCharts.hbs.ts
@@ -1,51 +1,29 @@
 import css from '../chat.css';
-import summerAvatar from '../../../../static/images/avatars/1.jpg';
-import jessicaAvatar from '../../../../static/images/avatars/3.jpg';
-import rickAvatar from '../../../../static/images/avatars/4.jpg';
-import avatar from '../../../../static/images/avatars/2.jpg';
-import { searchIcon, settingIcon } from '../../../utils/icons';
+import avatar from '../../../../static/images/avatars/4.jpg';
+import { searchIcon } from '../../../utils/icons';
+import { staticUrl } from '../../../api/api';
 
 export default function userChartsHbs(): string {
-  const charts = [
-    {
-      name: 'Summer',
-      message: 'B*tch, my generation gets traumatized for breakfast!',
-      avatar: summerAvatar,
-      unread: 2,
-      active: false,
-    },
-    {
-      name: 'Jessica',
-      message: 'Nobody exists on purpose. Nobody belongs anywhere. We’re all going to die.',
-      avatar: jessicaAvatar,
-      unread: 0,
-      active: false,
-    },
-    {
-      name: 'Rick',
-      message:
-        'Morty, I need your help on an adventure. Eh, ‘need’ is a strong word. We need door stops, but a brick would work too.',
-      avatar: rickAvatar,
-      unread: 5,
-      active: true,
-    },
-  ];
-
   return `
     <!-- user -->
     <div class="${css.userSettingsContainer}">
       <div class="${css.userSettingsAvatar}">
-        <img alt="Morty" src="${avatar}" class="${css.userSettingsAvatarImg}">
+        <img alt="avatar" src="${staticUrl}{{user.avatar}}" class="${css.userSettingsAvatarImg}">
       </div>
       <div class="${css.userSettings}">
         <div class="${css.userSettingsChildren}">
           <div>
-            {{> typography tag="h5" text="Morty"}}
+            {{#if user.displayName}}
+              {{> typography tag="h5" text=user.displayName}}
+            {{else}}
+              {{> typography tag="h5" text=user.firstName}}
+            {{/if}}
             {{> typography tag="h6" text="Hi Mr. Jellybean, I’m Morty. I’m on an adventure with my grandpa."}}
           </div>
-          <button class="${css.button} ${css.buttonIcon}" type="button">
-            ${settingIcon}
-          </button>
+          <div>
+            {{{logOut}}}
+            {{{settingLink}}}
+          </div>
         </div>
       </div>
     </div>
@@ -62,26 +40,27 @@ export default function userChartsHbs(): string {
     <!-- charts -->
     <div class="${css.chartTitle}">
       {{> typography tag="h3" text="Charts"}}
+      {{{addChat}}}
     </div>
-    <div class="${css.chartList}">
-      ${charts
-        .map((item) => {
-          return `
-          <div class="${css.chartContainer} ${item.active && css.chartContainerActive}">
-            <div class="${css.chartImageContainer}">
-              <div class="${css.chartImage}">
-                <img alt="Summer" src="${item.avatar}">
-              </div>
+    <div class="${css.chartList}" id="chats">
+      {{#each chats}}
+        <div class="${css.chartContainer}" data-chat-id="{{this.id}}"> <!-- {item.active && css.chartContainerActive}-->
+          <div class="${css.chartImageContainer}">
+            <div class="${css.chartImage}">
+              {{#if this.avatar}}
+                <img alt="avatar" src="${staticUrl}{{this.avatar}}">
+              {{else}}
+                <img alt="avatar" src="${avatar}">
+              {{/if}}
             </div>
-            <div class="${css.userContainer}">
-              {{> typography tag="h5" text="${item.name}"}}
-              <div class="${css.lastMessage}">${item.message}</div>
-            </div>
-            ${item.unread ? `<span class="${css.unreadCount}">${item.unread}</span>` : ''}
           </div>
-        `;
-        })
-        .join('')}
+          <div class="${css.userContainer}">
+            {{> typography tag="h5" text=this.title}}
+            <div class="${css.lastMessage}">{{this.lastMessage.content}}</div>
+          </div>
+          {{#if this.unreadCount}}<span class="${css.unreadCount}">{{this.unreadCount}}</span>{{/if}}
+        </div>
+      {{/each}}
     </div>
   `;
 }
